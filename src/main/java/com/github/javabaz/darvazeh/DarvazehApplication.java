14eb2aec5b8a0bcfc220cda1566ac44ac93e2184
Amin <Amin98Dev@gmail.com>
2024-09-24 09:38:43 +0330

    initial commit

---
+++ src/main/java/com/github/javabaz/darvazeh/DarvazehApplication.java
@@ -0,0 +1,13 @@
+package com.github.javabaz.darvazeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarvazehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarvazehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/javabaz/darvazeh/DarvazehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.javabaz.darvazeh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarvazehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
