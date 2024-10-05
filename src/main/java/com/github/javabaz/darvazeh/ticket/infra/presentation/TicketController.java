9a45c77ad2855014686a46b72c6d7d8f42efb411
ali <ali77erfagh99@gmail.com>
2024-10-05 16:02:42 +0330

    add buy ticket

--- src/main/java/com/github/javabaz/darvazeh/ticket/infra/presentation/TicketController.java
+++ src/main/java/com/github/javabaz/darvazeh/ticket/infra/presentation/TicketController.java
@@ -1,6 +1,8 @@
 package com.github.javabaz.darvazeh.ticket.infra.presentation;
 
 import com.github.javabaz.darvazeh.ticket.domin.usecase.AddTicketByOrganization;
+import com.github.javabaz.darvazeh.ticket.domin.usecase.BuyTicket;
+import com.github.javabaz.darvazeh.ticket.infra.presentation.dto.BuyTicketRequest;
 import com.github.javabaz.darvazeh.ticket.infra.presentation.dto.TicketRequest;
 import com.github.javabaz.darvazeh.ticket.infra.presentation.dto.TicketResponse;
 import jakarta.validation.Valid;
@@ -16,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketController {
     private final AddTicketByOrganization addTicketByOrganization;
 
+    private final BuyTicket buyTicket;
+
     //TODO just admin can access  to change add it
     @PostMapping
     public TicketResponse addTicket(@RequestBody @Valid TicketRequest request) {
         return addTicketByOrganization.addTicket(request);
+    }
+
+    @PostMapping("/buy")
+    TicketResponse buyTicket(@RequestBody @Valid BuyTicketRequest request) {
+        return buyTicket.buyTicket(request);
     }
 }
--- src/main/java/com/github/javabaz/darvazeh/ticket/infra/presentation/dto/BuyTicketRequest.java
+++ src/main/java/com/github/javabaz/darvazeh/ticket/infra/presentation/dto/BuyTicketRequest.java
@@ -1,11 +1,13 @@
 package com.github.javabaz.darvazeh.ticket.infra.presentation.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class BuyTicketRequest extends TicketRequest {
+    @NotNull
     private Long ticketId;
 
 }
