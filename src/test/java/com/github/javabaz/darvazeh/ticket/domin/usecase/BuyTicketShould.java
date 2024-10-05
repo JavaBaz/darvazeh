5ff1b49ff7c83567ca68a650923a11c6449f1dc0
ali <ali77erfagh99@gmail.com>
2024-10-05 10:54:00 +0330

    add new test

--- src/test/java/com/github/javabaz/darvazeh/ticket/domin/usecase/BuyTicketShould.java
+++ src/test/java/com/github/javabaz/darvazeh/ticket/domin/usecase/BuyTicketShould.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.Test;
 class BuyTicketShould {
 
     @Test
+    public void add_when_have_specified_type_and_price() {
+
+    }
+
+    @Test
+
     public void valid_when_user_just_have_one_type_of_ticket_and_one_ticket() {
 
     }
