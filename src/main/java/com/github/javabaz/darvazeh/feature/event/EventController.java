9a0a285979f8a82b4184f8b5dcb68b7af86d9f69
ali <ali77erfagh99@gmail.com>
2024-10-05 09:22:09 +0330

    add method find by name for controller

--- src/main/java/com/github/javabaz/darvazeh/feature/event/EventController.java
+++ src/main/java/com/github/javabaz/darvazeh/feature/event/EventController.java
@@ -1,6 +1,10 @@
 package com.github.javabaz.darvazeh.feature.event;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/events")
 public class EventController {
     private final EventService eventService;
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Event> getByName(@PathVariable String name) {
+        Event eventFound = eventService.getByName(name);
+        return new ResponseEntity<>(eventFound, HttpStatus.OK);
+    }
 }
