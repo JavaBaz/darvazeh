bbc815a631492d86f6859bcace3b2183b5526519
ali <ali77erfagh99@gmail.com>
2024-10-07 08:47:42 +0330

    add repository otp

--- src/main/java/com/github/javabaz/darvazeh/common/auth/otp/OtpUtil.java
+++ src/main/java/com/github/javabaz/darvazeh/common/auth/otp/OtpUtil.java
@@ -1,17 +1,26 @@
 package com.github.javabaz.darvazeh.common.auth.otp;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class OtpUtil {
+
+    private final OtpRepository otpRepository;
 
     public void sendOtpSms(String phoneNumber, String otp) {
         System.out.println("Sending OTP Sms to: " + phoneNumber + " - OTP : " + otp);
     }
 
 
-    public String generateOtp(){
+    public String generateOtp() {
         return "1234";
+    }
+
+    public boolean isValid()
+    {
+        return false;
     }
 }
